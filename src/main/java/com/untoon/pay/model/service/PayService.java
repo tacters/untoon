eb58cc64f099c6a6870dac9f2688339a0cb9ff65
nearer3 <nearer3@naver.com>
2021-02-17 17:08:29 +0900

    pay vo, payservice

--- src/main/java/com/untoon/pay/model/service/PayService.java
+++ src/main/java/com/untoon/pay/model/service/PayService.java
@@ -1,5 +1,15 @@
 package com.untoon.pay.model.service;
 
-public class PayService {
+import java.util.ArrayList;
 
+import com.untoon.pay.model.vo.Pay;
+
+public interface PayService {
+	ArrayList<Pay> payList(); //관리자용 결제목록조회
+	Pay detailPay(int payno); //관리자용 결제 상세보기
+	ArrayList<Pay> selectMyPay(); //마이페이지에서 사용자가 결제내역보기
+	Pay detailMyPay(int payno);
+	int insertPay(Pay pay);
+	int updatePay(Pay pay);
+	
 }
--- src/main/java/com/untoon/pay/model/vo/Pay.java
+++ src/main/java/com/untoon/pay/model/vo/Pay.java
@@ -1,5 +1,90 @@
 package com.untoon.pay.model.vo;
 
-public class Pay {
+import java.sql.Date;
 
+public class Pay implements java.io.Serializable {
+	private static final long serialVersionUID = 2222L;
+	
+	private int payno;
+	private String pid;
+	private int cid;
+	private Date paydate;
+	private String method;
+	private int pstatus;
+	
+	
+	
+	public Pay() {}
+
+	public Pay(int payno, String pid, int cid, Date paydate, String method, int pstatus) {
+		super();
+		this.payno = payno;
+		this.pid = pid;
+		this.cid = cid;
+		this.paydate = paydate;
+		this.method = method;
+		this.pstatus = pstatus;
+	}
+
+	public int getPayno() {
+		return payno;
+	}
+
+	public void setPayno(int payno) {
+		this.payno = payno;
+	}
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public Date getPaydate() {
+		return paydate;
+	}
+
+	public void setPaydate(Date paydate) {
+		this.paydate = paydate;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public int getPstatus() {
+		return pstatus;
+	}
+
+	public void setPstatus(int pstatus) {
+		this.pstatus = pstatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Pay [payno=" + payno + ", pid=" + pid + ", cid=" + cid + ", paydate=" + paydate + ", method=" + method
+				+ ", pstatus=" + pstatus + "]";
+	}
+	
+	
+	
+	
 }
