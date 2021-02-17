e543a0d494da5d756d24be9ad135e1195335d561
nearer3 <nearer3@naver.com>
2021-02-17 17:41:22 +0900

    payserviceimpl

--- src/main/java/com/untoon/pay/model/service/PayServiceImpl.java
+++ src/main/java/com/untoon/pay/model/service/PayServiceImpl.java
@@ -1,5 +1,53 @@
 package com.untoon.pay.model.service;
 
-public class PayServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.untoon.pay.model.dao.PayDao;
+import com.untoon.pay.model.vo.Pay;
+
+@Service("payService")
+public class PayServiceImpl implements PayService{
+	@Autowired
+	private PayDao payDao;
+	
+	@Override
+	public ArrayList<Pay> payList() {
+		
+		return null;
+	}
+
+	@Override
+	public Pay detailPay(int payno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Pay> selectMyPay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pay detailMyPay(int payno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertPay(Pay pay) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updatePay(Pay pay) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 }
