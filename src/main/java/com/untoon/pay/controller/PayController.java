55b9d626a7abbacb5306782e39dc4f7fb3122d6d
nearer3 <nearer3@naver.com>
2021-02-19 18:26:59 +0900

    pay controller insert기능 작성중입니다. 보성언니 결제 헬프미.. 일단 해보는 데까지 열시미 팔게여 ㅎㅎ

--- src/main/java/com/untoon/pay/controller/PayController.java
+++ src/main/java/com/untoon/pay/controller/PayController.java
@@ -15,8 +15,7 @@ import com.untoon.member.model.vo.Member;
 import com.untoon.pay.model.service.PayService;
 import com.untoon.pay.model.vo.Pay;
 
-@Controller   
-
+@Controller
 public class PayController {
 	
 	@Autowired
@@ -28,11 +27,11 @@ public class PayController {
 		
 		ArrayList<Pay> list = payService.payList();
 		Member loginUser = (Member) session.getAttribute("loginUser");
-		if(list.size() > 0 && loginUser != null && loginUser.getId().equals("A")) {
+		if(list.size() > 0 && loginUser != null && loginUser.getUser_lv().equals("A")) {
 			model.addAttribute("list", list);
 			return "pay/AdminPayList";
 		} else {
-			model.addAttribute("msg", "등록된 새 결제내역이 없습니다.");
+			model.addAttribute("msg", "결제내역 목록을 조회할 수 없습니다.");
 			return "common/errorPage";
 		}
 	}
@@ -42,6 +41,70 @@ public class PayController {
 	public String detailPay(@RequestParam("payno") int payno, Model model,
 				HttpSession session) {
 		Pay pay = payService.detailPay(payno);
+			
+		Member loginUser = (Member) session.getAttribute("loginUser");
+		if (pay !=null && loginUser != null && loginUser.getUser_lv().equals("A")){
+			model.addAttribute("pay", pay);
+			return "pay/AdminPayDetail";
+		} else {
+			model.addAttribute("msg", payno + "번 결제내역 상세보기를 할 수 없습니다.");
+			return "common/errorPage";
+		}	
+	}
+	
+	//사용자가 마이페이지에서 결제내역 목록을 조회할 때 
+	@RequestMapping("pmselect.do")
+	public String selectMyPay(Model model, HttpSession session) {
+		
+		ArrayList<Pay> list = payService.selectMyPay();
+		Member loginUser = (Member) session.getAttribute("loginUser");
+		if(list.size() > 0 && loginUser != null && loginUser.getUser_lv().equals("S")) {
+			model.addAttribute("list", list);
+			return "pay/MyPagePayList";
+		} else {
+			model.addAttribute("msg", "결제내역 목록을 조회할 수 없습니다.");
+			return "common/errorPage";
+		}
 	}
  
+	//사용자가 마이페이지에서 결제목록 상세보기 할 때 
+		@RequestMapping("pmdetail.do")
+		public String detailMyPay(@RequestParam("payno") int payno, Model model,
+					HttpSession session) {
+			Pay pay = payService.detailPay(payno);
+				
+			Member loginUser = (Member) session.getAttribute("loginUser");
+			if (pay !=null && loginUser != null && loginUser.getUser_lv().equals("S")){
+				model.addAttribute("pay", pay);
+				return "pay/MyPagePayDetail";
+			} else {
+				model.addAttribute("msg", payno + "번 결제내역 상세보기를 할 수 없습니다.");
+				return "common/errorPage";
+			}	
+		}
+	
+	// 결제목록 추가하기 
+		@RequestMapping("pinsert.do")
+		public String insertPay(Pay pay, Model model, int payno) {
+			if(payService.insertPay(pay) > 0) {
+				ArrayList<Pay> list =payService.selectMyPay(payno)
+				model.addAttribute("msg", "결제주문 성공");
+				return "pay/PayFinishPage";
+				
+			} else {
+				model.attribute("msg", "결제주문등록 실패 ");
+				return "common/errorPage";
+			
+			}
 }
+	// 결제목록 결제상태(결제승인여부) 업데이트
+		@RequestMapping("pupdate.do")
+		public String updatePay(Pay pay, Model model) {
+			if(payService.insertPay(pay) > 0)
+				return "pay/MyPayPageDetail";
+			} else {
+				model.attribute("msg", "결제업데이트 실패 ");
+				return "common/errorPage";
+				
+			}	
+} 
